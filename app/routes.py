1a4b70dda50b09e4c5c1711e6e337009f13a73e5
Tomasz Gerono <tomasz.gerono@gmail.com>
2024-06-05 16:41:22 +0200

    2024-06-05 TG

--- app/routes.py
+++ app/routes.py
@@ -9,10 +9,9 @@ import numpy as np
 from io import BytesIO
 from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
-import matplotlib
+import matplotlib 
 import xlsxwriter
-matplotlib.use('Agg')
-
+matplotlib.use('Agg') 
 
 @app.route('/')
 def index():
@@ -30,19 +29,13 @@ def extract():
                 opinions_count = page_dom.select_one("a.product-review__link > span").get_text().strip()
             except AttributeError:
                 opinions_count = 0
-
             if opinions_count:
                 product_name = page_dom.select_one("h1").get_text().strip()
-                url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
                 all_opinions = []
                 while(url):
                     print(url)
                     response = requests.get(url)
                     page_dom = BeautifulSoup(response.text, "html.parser")
-                    try:
-                        url = "https://www.ceneo.pl"+utils.extract(page_dom, "a.pagination__next", "href")
-                    except TypeError:
-                        url = None
                     opinions = page_dom.select("div.js_product-review")
                     for opinion in opinions:
                         single_opinion = {
@@ -52,7 +45,10 @@ def extract():
                         for key, value in utils.transformations.items():
                             single_opinion[key] = value(single_opinion[key])
                         all_opinions.append(single_opinion)
-                    
+                    try:
+                        url = "https://www.ceneo.pl"+utils.extract(page_dom, "a.pagination__next", "href")
+                    except TypeError:
+                        url = None
                 if not os.path.exists("app/opinions"):
                     os.mkdir("app/opinions")
                 jf = open(f"app/opinions/{product_id}.json", "w", encoding="UTF-8")
@@ -68,40 +64,39 @@ def extract():
                 score_distribution = opinions.score.value_counts().reindex(np.arange(0,5.5,0.5), fill_value = 0)
                 recommendation_distribution = opinions.recommendation.value_counts(dropna=False).reindex([True, False, np.nan], fill_value = 0)
                 product = {
-                    "product_id" : product_id,
-                    "product_name" : product_name,
-                    "opinions_count" : int(opinions_count),
-                    "pros_count" : int(pros_count),
-                    "cons_count" : int(cons_count),
-                    "average_score" : average_score,
-                    "score_distribution" : score_distribution.to_dict(),
-                    "recommendation_distribution" : recommendation_distribution.to_dict()
+                    'product_id': product_id,
+                    'product_name': product_name,
+                    'opinions_count': int(opinions_count),
+                    'pros_count': int(pros_count),
+                    'cons_count': int(cons_count),
+                    'average_score': average_score,
+                    'score_distribution': score_distribution.to_dict(),
+                    'recommendation_distribution': recommendation_distribution.to_dict()
                 }
-
                 if not os.path.exists("app/products"):
                     os.mkdir("app/products")
                 jf = open(f"app/products/{product_id}.json", "w", encoding="UTF-8")
                 json.dump(product, jf, indent=4, ensure_ascii=False)
                 jf.close()
-
                 if not os.path.exists("app/charts"):
                     os.mkdir("app/charts")
                 fig, ax = plt.subplots()
                 score_distribution.plot.bar(color = "hotpink")
                 plt.xlabel("Number of stars")
                 plt.ylabel("Number of  opinions")
-                plt.title(f"Score histogram for {product_id} product")
+                plt.title(f"Score histogram for {product_name}")
                 plt.xticks(rotation = 0)
                 ax.bar_label(ax.containers[0], label_type='edge', fmt = lambda l: int(l) if l else "")
                 plt.savefig(f"app/charts/{product_id}_score.png")
+                plt.close()
                 recommendation_distribution.plot.pie(
                     labels = ["Recommend", "Not recommend", "Indifferent"],
                     label = "",
                     colors = ["forestgreen", "crimson", "silver"],
                     autopct = lambda l: "{:1.1f}%".format(l) if l else ""
                 )
-                plt.title(f"Recommendations shares for {product_id} product")
-                plt.savefig(f"app/charts/{product_id}_recommendations.png")
+                plt.title(f"Recommendations shares for {product_name}")
+                plt.savefig(f"app/charts/{product_id}_recommendation.png")
                 return redirect(url_for('product', product_id=product_id))
             return render_template("extract.html", error = "Product has no opinions")
         return render_template("extract.html", error = "Product does not exist")
@@ -109,15 +104,15 @@ def extract():
 
 @app.route('/products')
 def products():
-    if os.path.exists("app/products"):
+    if os.path.exists("app/opinions"):
         products = [filename.split(".")[0] for filename in os.listdir("app/opinions")]
-    else: products = []    
+    else: products = []
     products_list = []
     for product in products:
-        jf = open(f"app/products/{product}.json","r",encoding="UTF-8")
+        jf = open(f"app/products/{product}.json", "r", encoding="UTF-8")
         single_product = json.load(jf)
         products_list.append(single_product)
-    return render_template("products.html",products = products_list)
+    return render_template("products.html", products = products_list)
 
 @app.route('/author')
 def author():
@@ -125,14 +120,12 @@ def author():
 
 @app.route('/product/<product_id>')
 def product(product_id):
-    opinions = pd.read_json(f"app/opinions/{products_id}.json")
-    return render_template("product.html", product_id=product_id, opinions=opinions_to_html(classes="table table-hover table-stripped", index=False))
-    index = False, table_id
+    opinions = pd.read_json(f"app/opinions/{product_id}.json")
+    return render_template("product.html", product_id=product_id, opinions=opinions.to_html(classes="table table-hover table-stripped", index=False, table_id = "opinions"))
 
 @app.route('/charts/<product_id>')
-def product(product_id):
-    return render_template("charts.html", product )
-    
+def charts(product_id):
+    return render_template("charts.html", product_id=product_id)
 
 @app.route('/download/json/<product_id>')
 def download_json(product_id):
@@ -140,16 +133,16 @@ def download_json(product_id):
 
 @app.route('/download/csv/<product_id>')
 def download_csv(product_id):
-    opinions = pd.read_json(f'app/opinions/{product_id}.json')
-    content_pl, content_en = opinions = ('content').apply(pd.Series)
+    opinions = pd.read_json(f"app/opinions/{product_id}.json")
+    opinions = pd.concat([opinions.drop(['content'], axis=1), opinions['content'].apply(pd.Series)], axis=1)
     response_stream = BytesIO(opinions.to_csv().encode())
     return send_file(response_stream, mimetype='text/csv', download_name=f'{product_id}.csv', as_attachment=True)
 
 @app.route('/download/xlsx/<product_id>')
 def download_xlsx(product_id):
-    opinions = pd.read_json(f'app/opinions/{product_id}.json')
+    opinions = pd.read_json(f"app/opinions/{product_id}.json")
     buffer = BytesIO()
-    with pd.ExcelWriter(buffer) as writer:
+    with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
         opinions.to_excel(writer)
     buffer.seek(0)
     return send_file(buffer, mimetype='application/vnd.ms-excel', download_name=f'{product_id}.xlsx', as_attachment=True)
